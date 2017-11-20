6d78680b4fabf96bcb4924e5f10cd23ef49be7d0
aurorex <selva_beat@hotmail.com>
2017-11-20 10:52:21 +0100

    agregando estilos

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ window.addEventListener('load', function() {
     listContainer.replaceChild(input, text);
     div.classList.add('div-style');
     input.classList.add('input-style');
+    input.setAttribute('placeholder', 'Añadir una lista...');
     button.classList.add('button-style');
     div.appendChild(input);
     div.appendChild(button);
@@ -26,15 +27,18 @@ window.addEventListener('load', function() {
     var valueInput = document.getElementsByClassName('input-style')[0].value;
     var saveText = document.createTextNode(valueInput);
     var newText = document.createElement('p');
+    newText.classList.add('newText-style');
     newText.appendChild(saveText);
     containerTareas.appendChild(newText);
     var anchor = document.createElement('a');
     anchor.setAttribute('href', '#');
     anchor.setAttribute('id', 'anchor-id');
+    anchor.classList.add('anchor-style');
     var textAnchor = document.createTextNode('Añadir una tarea');
     anchor.appendChild(textAnchor);
     containerTareas.appendChild(anchor);
     save.appendChild(containerTareas);
+    save.classList.add('save-style');
 
     anchor.addEventListener('click', anchorList);
 
@@ -42,18 +46,21 @@ window.addEventListener('load', function() {
       var textArea = document.createElement('textarea');
       textArea.setAttribute('rows', '3');
       textArea.setAttribute('cols', '30');
+      textArea.setAttribute('placeholder', 'Añadir una tarea');
       textArea.setAttribute('class', 'textarea-class');
       containerTareas.replaceChild(textArea, anchor);
       var newButton = document.createElement('button');
+      newButton.classList.add('newButton-style');
       var textButton = document.createTextNode('Añadir');
       newButton.appendChild(textButton);
       containerTareas.appendChild(newButton);
 
       newButton.addEventListener('click', positionList);
-      
+
       function positionList(event) {
         var valueTextarea = textArea.value;
         var newPharraphe = document.createElement('p');
+        newPharraphe.classList.add('newPharraphe-style');
         var textPharraphe = document.createTextNode(valueTextarea);
         newPharraphe.appendChild(textPharraphe);
         containerTareas.insertBefore(newPharraphe, textArea);
